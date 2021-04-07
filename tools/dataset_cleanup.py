22a22b9b4b9b9c59588c882651bd6efd74cb90c3
Smeefer <filip.pagliaro.4567@student.uu.se>
2021-04-07 10:42:22 +0200

    moved data

--- tools/dataset_cleanup.py
+++ tools/dataset_cleanup.py
@@ -3,7 +3,7 @@ import seaborn as sea # plotting
 import cv2 # image reading
 
 
-df = pd.read_excel('../ageGender.xlsx')
+df = pd.read_excel('../data/ageGender.xlsx')
 modified = df[['ImageFrontID', 'Kon']]
 modified.columns = ['image', 'gender']
 modified['image'] = modified['image'].str.replace('A', '')
@@ -12,4 +12,4 @@ modified['image'] = modified['image'].str.replace('A', '')
 
 print(df[df.gender == 'K'].shape[0])
 
-modified.to_csv('dataset.csv', index=False, sep=';')
+modified.to_csv('../data/dataset.csv', index=False, sep=';')
