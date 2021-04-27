ac162dd0281568b6a8404f651fc29c9ea91a29fc
ulrikabremberg <ulrika.bremberg@hotmail.com>
2021-04-27 17:43:42 +0200

    Started work on deno server

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd # data
 import seaborn as sea # plotting
 import cv2 # image reading
 
-#df = pd.read_csv('data/dataset.csv', sep=';')
+df = pd.read_csv('../data/dataset.csv', sep=';')
 
 hej = df.loc[df['image'] == '001.jpg']
-print(hej.loc[image])
+print(hej)
