e3189fd023246e450d9a1bb493c40b9e3e09738e
ulrikabremberg <ulrika.bremberg@hotmail.com>
2021-05-11 13:37:24 +0200

    Added functionality for augmenting the images in data.py

--- model/data.py
+++ model/data.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
 def data(dataset, image_folder, img_shape, test_size, logging):
     # Depending on how many channels the image is in color or not
@@ -40,5 +41,28 @@ def data(dataset, image_folder, img_shape, test_size, logging):
     y = np.array(labels_one_hot)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    datagen = ImageDataGenerator(
+        rotation_range=30,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        #shear_range=0.2,
+        #zoom_range=0.3,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    index = 0
+    for image in X_train:
+        x = image.reshape((1,) + image.shape)
+
+        i = 0
+        for batch in datagen.flow(x, batch_size=1):
+            i += 1
+            X_train = np.concatenate((X_train, batch))
+            y_train = np.append(y_train, [y_train[index]], axis=0)
+            if i > 4:
+                print('done with 1 batch')
+                break
+        index+=1
 
     return X_train, X_test, y_train, y_test
