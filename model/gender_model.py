dd80777b665460f4dc3682b1bd3103a6aa38a27d
FilPag <filip.pagliaro.4567@student.uu.se>
2021-05-13 14:40:20 +0200

    new gender model

---
+++ model/gender_model.py
@@ -0,0 +1,89 @@
+from tensorflow.keras.layers import Dense, MaxPooling2D, Conv2D, Dropout, Flatten, Input, BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam
+
+
+def Convolution(input, filters):
+    x = Conv2D(filters=filters, kernel_size=(3, 3), activation='relu',
+               padding='same', strides=(1, 1), kernel_regularizer=l2(0.001))(input)
+    x = Dropout(0.2)(x)
+    return x
+
+
+def create_model(input_shape):
+    inputs = Input(input_shape)
+
+    '''
+    x = Convolution(inputs, 16)
+    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
+    #x = BatchNormalization(x, axis = 1)
+
+    x = Convolution(inputs, 32)
+    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
+    #x = BatchNormalization(x, axis = 1)
+
+    x = Convolution(x, 64)
+    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
+    '''
+    #x = BatchNormalization(x, axis = 1)
+
+    x = Convolution(inputs, 128)
+    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
+    #x = BatchNormalization(x,)
+
+    x = Convolution(x, 256)
+    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
+
+    x = Flatten()(x)
+    x = Dropout(0.8)(x)
+
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.5)(x)
+
+    output = Dense(2, activation="softmax")(x)
+
+    model = Model(inputs=inputs, outputs=output)
+    #opt = Adam(lr=0.1)
+    model.compile(loss=["binary_crossentropy"],
+                  optimizer='adam', metrics=["accuracy"])
+
+    return model
+
+
+def train_gender_model(X_train, X_test, y_train, y_test, img_shape, batch_size, epochs, model_save, monitor='val_loss'):
+
+    model = create_model(img_shape)
+    #model.summary()
+
+
+    checkpointer = ModelCheckpoint(
+        filepath=model_save,
+        monitor=monitor,
+        verbose=0,
+        save_best_only=True,
+        save_weights_only=False,
+        mode='auto',
+        save_freq='epoch'
+    )
+    Early_stop = EarlyStopping(
+        patience=25,
+        monitor=monitor,
+        restore_best_weights=True
+    )
+    callback_list = [Early_stop, checkpointer]
+
+    model.fit(
+        X_train,
+        y_train,
+        batch_size=batch_size,
+        validation_data=(X_test, y_test),
+        epochs=epochs,
+        verbose=1,
+        callbacks=[callback_list]
+    )
+    return model
---
+++ model/train_model.py
@@ -0,0 +1,73 @@
+import sys
+from gender_model import train_gender_model
+from age_model import train_age_model
+from data import data_gender, data_age
+import numpy as np
+
+# Settings
+image_folder = "resized_96_equal_distribution_pictures_grayscale"
+full_dataset_folder = "../dataset"
+dataset = "../data/full_dataset.csv"
+test_size = 0.25
+img_shape = (96, 96, 1)
+logging = False
+gender_model_save = 'g_bw_final.h5'
+age_model_save = 'a_final.h5'
+batch_size = 64
+epochs = 300
+multiple_runs = True
+monitor = 'val_loss'
+augumentation = False
+
+# train gender model
+def gender():
+    X_train, X_test, y_train, y_test = data_gender(dataset, image_folder, img_shape, test_size, augumentation, logging)
+    return X_test, y_test, train_gender_model(X_train, X_test, y_train, y_test, img_shape, batch_size, epochs, gender_model_save, monitor)
+# Train age model
+
+
+def age(equal = False):
+    X_train, X_test, y_train, y_test = data_age(dataset, full_dataset_folder, equal, img_shape, test_size, augumentation, logging)
+    return X_test, y_test, train_age_model(X_train, X_test, y_train, y_test, img_shape, batch_size, epochs, age_model_save, monitor)
+
+type_m = 'B'
+
+if len(sys.argv) > 1:
+    type_m = sys.argv[1].upper()
+
+if type_m == 'G':
+    X_test, y_test, gender_model = gender()
+    score = gender_model.evaluate(X_test, y_test)[1]
+    print('Gender test accuracy: ', score)
+    # GÖR VAFAN DU VILL HÄR :)
+
+elif type_m == 'A':
+    X_test, y_test, age_model = age()
+    score = age_model.evaluate(X_test, y_test)[1]
+    print('Age test accuracy: ', score)
+
+elif type_m == 'B':
+    X_test_gender, y_test_gender, gender_model = gender()
+    X_test_age, y_test_age,  age_model = age()
+
+    gender_score = gender_model.evaluate(X_test_gender, y_test_gender)[1]
+    age_score = age_model.evaluate(X_test_gender, y_test_gender)[1]
+    print('Mean Age Error: ', age_score)
+    print('Gender accuracy: ', gender_score)
+
+    score = gender_score
+
+    # GÖR VAFAN DU VILL HÄR :)
+
+else:
+    print('Usage: python run_model.py {A/G}')
+    quit()
+
+
+# Train model
+
+# This appends the accuracy to results.txt if doing multiple runs
+if multiple_runs:
+    f = open("results.txt", "a")
+    f.write(str(score)+'\n')
+    f.close()
